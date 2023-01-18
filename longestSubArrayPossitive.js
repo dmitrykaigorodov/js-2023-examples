1f853d623e5e5ed384e90c072b952b3792689f17
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2023-01-18 01:15:32 +0100

    fixed solution from YouTube

--- longestSubArrayPossitive.js
+++ longestSubArrayPossitive.js
@@ -1,40 +1,29 @@
-const arr = [-1, 1, 0, 1, 2, 0, -1, 1, 2, 3, -10];
+const arr = [-1, 1, 0, 3, 2, 0, -1, 7, 5, 4, -10];
 /**
  * return the longest sub-array of positive for a given array
- *@param{number[]} arr
+ * @param{number[]} array
+ * @returns{number[]}
  */
 const longestSubArrayPossitive = (array) => {
   let positiveCount = 0;
   let maxCount = 0;
-  let arrayOfPositiveNumbers_key = [];
-  let emptyArrforI = [];
-  let subArray = [];
+  let currentPositives = [];
+  let maxCurruntPositives = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0) {
       positiveCount += 1;
-      if (positiveCount > 0) {
+      currentPositives.push(array[i]);
+      if (positiveCount > maxCount) {
         maxCount = positiveCount;
-        arrayOfPositiveNumbers_key.push(array[i]);
-        maxCount;
+        maxCurruntPositives = currentPositives;
       }
     } else {
       positiveCount = 0;
+      currentPositives = [];
     }
-  }
-  let indexOf_i = arrayOfPositiveNumbers_key.indexOf(1);
-  while (indexOf_i != -1) {
-    emptyArrforI.push(indexOf_i);
-    indexOf_i = arrayOfPositiveNumbers_key.indexOf(1, indexOf_i + 1);
+    // console.log(i, array[i], positiveCount, maxCount, currentPositives, maxCurruntPositives);
   }
 
-  for (let i = 0; i < arrayOfPositiveNumbers_key.length / 2; i++) {
-    subArray[i] = arrayOfPositiveNumbers_key.slice(
-      emptyArrforI[i],
-      emptyArrforI[i + 1]
-    );
-  }
-  let compareSubArray = subArray.filter((el) => el.length === maxCount);
-  const finalArr = compareSubArray.reduce((acc, cur) => [...acc, ...cur]);
-  return finalArr;
+  return maxCurruntPositives;
 };
 console.log(longestSubArrayPossitive(arr));
